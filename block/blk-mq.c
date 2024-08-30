9433f28fd2337c82d246f76c9426281758ddd634
Keith Busch <kbusch@kernel.org>
2024-08-30 11:45:09 -0700

    blk-mq: set the nr_integrity_segments from bio
    This value is used for potential merging later.
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -2546,6 +2546,10 @@ static void blk_mq_bio_to_request(struct request *rq, struct bio *bio,
 	rq->__sector = bio->bi_iter.bi_sector;
 	rq->write_hint = bio->bi_write_hint;
 	blk_rq_bio_prep(rq, bio, nr_segs);
+#if defined(CONFIG_BLK_DEV_INTEGRITY)
+	if (bio->bi_opf & REQ_INTEGRITY)
+		rq->nr_integrity_segments = blk_rq_count_integrity_sg(rq->q, bio);
+#endif
 
 	/* This can't fail, since GFP_NOIO includes __GFP_DIRECT_RECLAIM. */
 	err = blk_crypto_rq_bio_prep(rq, bio, GFP_NOIO);
